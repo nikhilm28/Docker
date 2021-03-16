0dbb03be5c568fa1aac0ce868461a660153cf06e
nikhilm28 <55309760+nikhilm28@users.noreply.github.com>
2021-03-16 22:37:00 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,9 @@
+use pucsdStudents;
+
+CREATE TABLE studentData (name varchar(20), roll_no int, address text, mobile_no numeric(10), pan_no varchar(10));
+
+insert into studentData values('Nikhil', 'N19111023', 'Vadgaon sheri pune', '9049084122', 'ABCDW1122E');
+insert into studentData values('Avinash', 'N1911004', 'Dehu Road', '866124589', 'ABCDE1012F');
+insert into studentData values('Sunny', 'N19111045', 'Ahmednagar', '2235648988', 'ABDEF1420R');
+insert into studentData values('Vishal', 'R1910003', 'Pune', '8149142225', 'ADEF1510N');
+
